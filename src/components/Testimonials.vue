cac6d48bbf45ce64b21db8fa49cb79a85d8ef0c1
Rico Monden <mond0040@algonqinlive.com>
2019-06-28 14:28:44 -0400

    finished setup

---
+++ src/components/Testimonials.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+Testimonialsasassasa
+</div>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+import Contact from './components/Contact.vue'
+import Testimonials from './components/Testimonials.vue'
 
 Vue.use(Router)
 
@@ -14,10 +17,25 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    },
+    {
+      path: '/testimonials',
+      name: 'testimonials',
+      component: Testimonials
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
